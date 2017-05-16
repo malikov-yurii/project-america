5f2975695e0ec4b094f857921837a5eef29705be
Foldaza <romanyshyn.sasha@gmail.com>
2017-05-16 21:37:00 +0300

    Basic methods for Triangle class

--- scr/Triangle.java
+++ scr/Triangle.java
@@ -1,4 +1,31 @@
 
 public abstract class Triangle {
-    //stub
+    private double sideA;
+    private double sideB;
+    private double sideC;
+
+    public Triangle(double a, double b, double c) {
+        this.sideA = a;
+        this.sideB = b;
+        this.sideC = c;
+    }
+
+
+    public double getPerimetr() {
+        return sideA + sideB + sideC;
+    }
+
+    public double getSideA() {
+        return sideA;
+    }
+
+    public double getSideB() {
+        return sideB;
+    }
+
+    public double getSideC() {
+        return sideC;
+    }
+
+    protected abstract String getColour();
 }
